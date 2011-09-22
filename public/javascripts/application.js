f86fc552dcee90f3341007d39a8fe6fa733359bb
Danny Peck <dannypeck@gmail.com>
2011-09-22 16:26:24 -0500

    Updates to header/footer. Fixes to javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,47 +1,51 @@
+$(document).ready(function(){
+  init_external_links();
+  init_parent_links();
+  init_pre_conversion();
+  init_home_class();
+});
+
 /* Scans the DOM for links with the
  * rel="external" attribute and
  * sets a click event to open said
  * link in a new window. */
-$(document).ready(function(){
+function init_external_links() {
   $("a[rel='external']").click(function() {
     window.open($(this).attr('href'));
     return false;
   });
-});
+}
 
 /* This is a Javascript-driven fix to target
  * link categories and make them "unclickable".
  * A class is also added that is targeted for CSS
  * purposes. */
-$(document).ready(function() {
-  $(".widget_pages li, #footer li").each(function() {
+function init_parent_links() {
+  $(".widget_pages li").each(function() {
     if (!$(this).parent().hasClass("children")) {
       $(this).addClass("parent");
       $(this).find("a").first().addClass("parent");
     }
   });
-  $(".widget_pages .parent a").first().click(function() {
+  $(".widget_pages a.parent").click(function() {
     return false;
   });
-  $("#footer a.parent").click(function() {
-    return false;
-  });
-});
-
+}
 
 /* Converting <pre> to <p> for friendlier pasting in to Wordpress */
-$(document).ready(function() {
+function init_pre_conversion() {
   $(".contents pre").each(function() {
     $(this).replaceWith($('<p>' + nl2br(this.innerHTML) + '</p>'));
   });
-})
+}
 function nl2br (str, is_xhtml) {
   var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
   return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
 }
 
-$(document).ready(function() {
+/* Adding a homepage class for css targeting */
+function init_home_class() {
   if ($(".contents h2").html().match("Home")) {
     $(".contents").addClass("home");
   }
-})
+}
